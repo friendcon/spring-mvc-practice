eb2d3648f7bf69d047273bf6816a5949cb0c25e9
con <imyhy01@gmail.com>
2021-08-15 21:05:26 +0900

    [test]

--- src/main/java/com/springmvc/controller/ReplyController.java
+++ src/main/java/com/springmvc/controller/ReplyController.java
@@ -50,7 +50,7 @@ public class ReplyController {
 	public ResponseEntity<List<ReplyVO>> getList(
 			@PathVariable("page") int page,
 			@PathVariable("bno") Long bno) {
-		log.info("getList. . . . . . ");
+		log.info("getList. . . . . . . ");
 		Criteria cri = new Criteria(page, 10);
 		log.info(cri);
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
